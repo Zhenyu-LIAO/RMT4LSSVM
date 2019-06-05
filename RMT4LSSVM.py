bf9007d044f9f56e95dba13dc6bdc7982fc6d957
Zhenyu LIAO <raymondedge928@gmail.com>
2019-06-05 16:22:18 +0200

    update Gaussian means

--- RMT4LSSVM.py
+++ RMT4LSSVM.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 
 # # A Large Dimenional Analysis of LS-SVM
@@ -6,6 +6,7 @@
 # by [Zhenyu Liao](http://zhenyu-liao.github.io/), CentraleSupelec, Paris-Saclay
 
 # In[1]:
+
 
 import scipy.special,scipy.linalg
 import numpy as np
@@ -18,7 +19,8 @@ from sklearn.datasets import fetch_mldata
 
 # In[2]:
 
-def get_data(testcase,n,n_test,prop,selected_target=None,means=None,covs=None,):
+
+def get_data(testcase,p,n,n_test,prop,selected_target,means,covs):
     rng = np.random
 
     # different cases for data
@@ -28,7 +30,6 @@ def get_data(testcase,n,n_test,prop,selected_target=None,means=None,covs=None,):
         X_train_full, X_test_full = X[:60000], X[60000:]
         y_train_full, y_test_full = y[:60000], y[60000:]
 
-        p = 784
         K=len(selected_target)
         X_train = np.array([]).reshape(p,0)
         X_test = np.array([]).reshape(p,0)        
@@ -59,7 +60,7 @@ def get_data(testcase,n,n_test,prop,selected_target=None,means=None,covs=None,):
         K = len(prop)
         for i in range(K):            
             X_train = np.concatenate((X_train,rng.multivariate_normal(means[i],covs[i],size=np.int(n*prop[i])).T),axis=1)
-            X_test  = np.concatenate((X_test, rng.multivariate_normal(means[i],covs[i],size=np.int(n_test*prop[i])).T),axis=1)
+            X_test  = np.concatenate((X_test,rng.multivariate_normal(means[i],covs[i],size=np.int(n_test*prop[i])).T),axis=1)
             y_train = np.concatenate( (y_train,2*(i-K/2+.5)*np.ones(np.int(n*prop[i]))) )
             y_test = np.concatenate( (y_test,2*(i-K/2+.5)*np.ones(np.int(n_test*prop[i]))) )            
             
@@ -72,6 +73,7 @@ def get_data(testcase,n,n_test,prop,selected_target=None,means=None,covs=None,):
 # ### Generate Kernel function $f$
 
 # In[3]:
+
 
 def get_kernel(kernel,z,derivs=None,tau=None,sigma2=None):
     if kernel in ['poly','poly_zero']:
@@ -95,6 +97,7 @@ def get_kernel(kernel,z,derivs=None,tau=None,sigma2=None):
 
 # In[4]:
 
+
 def get_stat(X,prop):
     p = X_train.shape[0]
     n = X_train.shape[1]
@@ -117,7 +120,8 @@ def get_stat(X,prop):
 
 # In[5]:
 
-testcase = 'MNIST'# testcase for simulation, among 'iid','means','var','orth','mixed','MNIST'
+
+testcase = 'means'# testcase for simulation, among 'iid','means','var','orth','mixed','MNIST'
 kernel = 'gauss'  # kernel used for LS-SVM, among 'gauss', 'poly', 'poly_zero'
 
 n = 256 # number of training samples 
@@ -140,8 +144,11 @@ for loop in range(loops):
     if testcase is 'MNIST':
         p=784
         selected_target = [3,8]
-        X_train,X_test,y_train,y_test = get_data(testcase,n,n_test,prop,selected_target)
-    else:    
+        means = []
+        covs = []
+        X_train,X_test,y_train,y_test = get_data(testcase,p,n,n_test,prop,selected_target,means,covs)
+    else:
+        selected_target = [0,1]
         means=[]
         covs=[]
         if testcase is 'iid':
@@ -164,8 +171,7 @@ for loop in range(loops):
             for i in range(k):
                 means.append( np.concatenate( (np.zeros(i),2*np.ones(1),np.zeros(p-i-1)) ) )
                 covs.append((1+4*i/np.sqrt(p))*scipy.linalg.toeplitz( [(.4*i)**x for x in range(p)] ))            
-
-        X_train,X_test,y_train,y_test = get_data(testcase,n,n_test,prop,means,covs)
+        X_train,X_test,y_train,y_test = get_data(testcase,p,n,n_test,prop,selected_target,means,covs)
 
     # computation of tau
     XX_train = X_train.T@X_train/p
@@ -226,22 +232,27 @@ var_th = 8*gamma**2*(prop[0]*prop[1])**2*np.array([V11+V21+V31, V12+V22+V32])
 
 # In[6]:
 
-g_mean = np.mean(g,0)
-    
-g1 = g_mean[range(int(n_test*prop[0]))]
-g2 = g_mean[int(n_test*prop[0]):]
 
-xs1 = np.linspace(min(g1.flatten()),max(g1.flatten()),30)
-xs2 = np.linspace(min(g2.flatten()),max(g2.flatten()),30)
+#g_mean = np.mean(g,0)
+    
+g1 = g[:,range(int(n_test*prop[0]))]
+g2 = g[:,int(n_test*prop[0]):]
+
+g1 = g1.flatten()
+g2 = g2.flatten()
+
+xs1 = np.linspace(min(g1),max(g1),30)
+xs2 = np.linspace(min(g2),max(g2),30)
 step1 = xs1[1]-xs1[0]
 step2 = xs2[1]-xs2[0]
-g_th1 = scipy.stats.norm.pdf((xs1-mean_th[0])/np.sqrt(var_th[0])).reshape(30,1)
-g_th2 = scipy.stats.norm.pdf((xs2-mean_th[1])/np.sqrt(var_th[1])).reshape(30,1)
 
-n1, bins1, patches1 ,= plt.hist(g1.flatten(), 30, facecolor='blue', alpha=0.75)
-n2, bins2, patches2 ,= plt.hist(g2.flatten(), 30, facecolor='red', alpha=0.75)
+g_th1 = scipy.stats.norm.pdf(xs1, loc=mean_th[0], scale=np.sqrt(var_th[0])).reshape(30,1)
+g_th2 = scipy.stats.norm.pdf(xs2, loc=mean_th[1], scale=np.sqrt(var_th[1])).reshape(30,1)
 
-pl1 ,=plt.plot(xs1,g_th1*n_test*loops*prop[0]*step1,'green')
-pl2 ,=plt.plot(xs2,g_th2*n_test*loops*prop[1]*step2,'purple')
+n1, bins1, patches1 ,= plt.hist(g1, 30, facecolor='blue', alpha=0.75)
+n2, bins2, patches2 ,= plt.hist(g2, 30, facecolor='red', alpha=0.75)
+
+pl1 ,=plt.plot(xs1,g_th1*n_test*prop[0]*step1*loops,'green')
+pl2 ,=plt.plot(xs2,g_th2*n_test*prop[1]*step2*loops,'purple')
 plt.show()
 
